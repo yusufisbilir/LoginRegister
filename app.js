62af24841cbc9d788fc5576d3a69155d8e584d26
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-28 16:04:57 +0300

    add dotenv, connected MySql database

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+//Npm modules
 const express = require('express');
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
-const port = 3000;
+
+//Database connection
+dbPass = process.env.DB_PASS;
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:dbPass,
+    database:'nodejs-login'
+});
+
+db.connect((err)=>{
+    if(err) console.log(err);
+    else console.log('connected MySql');
+});
 
 app.get('/', (req, res) => {
     res.send('Home page')
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(3000, () => console.log(`Example app listening on port 3000!`));
