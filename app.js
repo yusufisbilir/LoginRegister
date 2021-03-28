08c70bc30376a1b61da1c268959ac14ba6b942ff
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-28 15:08:50 +0300

    npm modules downloaded, created express server

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Home page')
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
