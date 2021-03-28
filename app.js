80d555fb9b7bbd4a1f79d104170b7d4cabac4a4a
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-28 17:43:39 +0300

    register, homepage routes

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 const express = require('express');
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
-const path = require('path');
-const { get } = require('http');
 
 dotenv.config();
 
@@ -27,12 +25,7 @@ db.connect((err)=>{
     else console.log('connected MySql');
 });
 
-app.get('/', (req, res) => {
-    res.render('index')
-});
+//routes
+app.use('/',require('./routes/pages'));
 
-app.get('/register', (req, res) => {
-    res.render('register')
-})
-
-app.listen(3000, () => console.log(`Example app listening on port 3000!`));
+app.listen(3000, () => console.log(`App listening on port 3000!`));
---
+++ routes/pages.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+module.exports = router;
